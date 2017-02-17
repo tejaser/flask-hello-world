b898934b28ccf605a2304e92123db85d7357c50d
Tejas Pancholi <tejas.er@outlook.com>
2017-02-17 12:31:36 +0530

    Add: Added DEBUG functionality

--- app.py
+++ app.py
@@ -14,7 +14,36 @@ app = Flask(__name__)
 @app.route("/hello")
 # define the view using a function, which returns a string
 def hello_world():
-    return "hello, world!"
+    return "hello, world!?!?!?!"
+
+# error handling
+app.config["DEBUG"] = True
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_queryat
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name), 200
+    else:
+        return 'Not Found', 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
